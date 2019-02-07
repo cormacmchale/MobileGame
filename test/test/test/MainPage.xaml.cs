8d0537c1dffd797d27c8ee7d6f6e58b8102597fd
unknown <cormacmchale1@gmail.com>
2019-02-07 12:42:39 +0000

    leaps and bounds

--- test/test/test/MainPage.xaml.cs
+++ test/test/test/MainPage.xaml.cs
@@ -7,70 +7,67 @@ namespace test
 {
     public partial class MainPage : ContentPage
     {
-        
-        static int test = 0;
-        Timer t = new Timer(1000);
+        static int test;
+        Timer t = new Timer(70);
+        static int movement;
 
+        //Avoid this object
+        BoxView b = new BoxView
+        {
+            HeightRequest = 10,
+            WidthRequest = 10,
+            Color = Color.Black,
+        };
         public MainPage()
         {
             InitializeComponent();
             t.Elapsed += T_Elapsed1;
             t.Start();
-
+            //b.SetValue(Grid.RowProperty, 0);
+            //Main.Children.Add(b);
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            t.Stop();
             test++;
-            Entry entry = FindByName("changeNum") as Entry;
-            entry.Text = test.ToString();
-         
+            changeNum.Text = test.ToString();     
+        }
+
+        private void B_SizeChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine("SizeChanged");
         }
 
         private void T_Elapsed1(object sender, ElapsedEventArgs e)
         {
             Device.BeginInvokeOnMainThread(() =>
             {
+                //move the pieces
                 movingGame();
+                //detect collisions
             }
-                        );
-
+            );
             //CoreDispatcher.
         }
-
+        //change movement
         private void Button_Clicked_1(object sender, EventArgs e)
         {
-            BoxView player = FindByName("Player") as BoxView;
-            player.TranslationX+=-4;
+            movement = 2;
         }
-
+        //change movement
         private void Button_Clicked_2(object sender, EventArgs e)
         {
-            BoxView player = FindByName("Player") as BoxView;
-            player.TranslationX+=4;
+            movement = -2;
         }
-
         private  void movingGame()
         {
             // BoxView player = FindByName("Player") as BoxView;
             Debug.WriteLine("Timer");
             test++;
-            BoxView b = new BoxView();
-            b.HeightRequest = 50;
-            b.WidthRequest = 50;
-            //b.Color = Windows.UI.Colors.Black;
-            //CoreDispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            ///() =>
-            //{
-            // Your UI update code goes here!
-            //});
-
-            
-
-            //changeNum.Text = test.ToString();
-            Player.TranslationX += -4;
-            //Entry entry = FindByName("changeNum") as Entry;
-            //entry.Text = test.ToString();
+            Player.TranslationX += movement;
+            Debug.WriteLine(Player.TranslationX);
+            //b.TranslationX += -2;
         }
     }
 }
