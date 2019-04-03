ae2eedd63a70b95a87fc13894cc54edc0e5cee14
unknown <cormacmchale1@gmail.com>
2019-04-03 16:55:47 +0100

    data binding attempt

--- test/test/test/MainPage.xaml.cs
+++ test/test/test/MainPage.xaml.cs
@@ -32,6 +32,7 @@ namespace test
         {
             InitializeComponent();
             InitilizeGame();
+            this.BindingContext = score;
         }
         private void InitilizeGame()
         {
--- test/test/test/scorePosition.cs
+++ test/test/test/scorePosition.cs
@@ -7,9 +7,30 @@ namespace test
 {
     class scorePosition : INotifyPropertyChanged
     {
-        private int score;
-        public int Score { get; set; }
-
         public event PropertyChangedEventHandler PropertyChanged;
+        private int _score;
+        public int Score {
+                    get
+                    {
+                        return _score;
+                    }
+                    set
+                    {
+                        if (_score == value) return; // no change
+                        _score = value;
+                        // notify the system of the change
+                        // there is a change, whatever is databound will update 
+                        // if PC == null, do nothing, else Invoke the
+                        // PC event handler with the two arguments
+                        //PropertyChanged?.Invoke(this,
+                        //    new PropertyChangedEventArgs(nameof(Size)));
+                        OnPropertyChanged(nameof(Score));
+                    }
+               }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
