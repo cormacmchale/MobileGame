c2f6b2042fd49be8a4be00e753eab45bb69ebf47
unknown <cormacmchale1@gmail.com>
2019-04-09 13:24:05 +0100

    game nearly finished

--- test/test/test/HighScoreReplay.xaml.cs
+++ test/test/test/HighScoreReplay.xaml.cs
@@ -14,8 +14,17 @@ namespace test
 	{
         //will contain al prevoius scores, will work like a score board
         List<scorePosition> highScore = new List<scorePosition>();
+        //backGround
+        Image space = new Image
+        {
+            Aspect = Aspect.AspectFill,
+            VerticalOptions = LayoutOptions.FillAndExpand,
+            HorizontalOptions = LayoutOptions.FillAndExpand
+        };
+        Imager getImage = new Imager();
+
         //takes in score from main page after collision with game objects
-		public HighScoreReplay (int Score)
+        public HighScoreReplay (int Score)
 		{
 			InitializeComponent();
             readScoresFromFile();
@@ -28,6 +37,15 @@ namespace test
         }
         private void setUpScoreBoard(int score)
         {
+                //not working at the moment adds on top of xaml
+                //same as previous pages backGround for nice UI
+                ///space.Source = getImage.AddImage("backGround.png");
+                //space.SetValue(Grid.ColumnSpanProperty, 9);
+                //space.SetValue(Grid.RowSpanProperty, 9);
+                //Main.Children.Add(space);
+                //unsure as to why this wont fill page - issue fixed
+                //this now added in xaml
+                //Main.Children.Add(space);
             //get the score from the main page
             scorePosition newScore = new scorePosition();
             //save it into a score object
